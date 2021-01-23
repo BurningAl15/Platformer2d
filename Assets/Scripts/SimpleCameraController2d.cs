bebce4e62356f5f12e24e641e25ef8bf3def638a
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-23 18:48:25 -0500

    Camera vertical clamping - Vertical parallax

---
+++ Assets/Scripts/SimpleCameraController2d.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraController2d : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [Header("Parallax Layers")]
+    [SerializeField] private Transform farBackground, middleBackground;
+    private Vector2 lastPos;
+
+    [Header("Camera Height Clamping")] 
+    [SerializeField] private float minHeight;
+    [SerializeField] private float maxHeight;
+    
+    private void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        //Horizontal Movement
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        //Clamping Vertical Camera
+        float clampedY = Mathf.Clamp(transform.position.y, minHeight, maxHeight);
+        transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
+
+        //Parallax - Horizontal (X) - Vertical (Y)
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x,transform.position.y - lastPos.y);
+
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0);
+        middleBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0) * .5f;
+        
+        lastPos = transform.position;
+    }
+}
