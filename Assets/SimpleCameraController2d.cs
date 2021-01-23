8b4e00e33245037b3cdc69cd8c97eb2b094faa58
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-23 17:11:02 -0500

    Simple Parallax

--- Assets/SimpleCameraController2d.cs
+++ Assets/SimpleCameraController2d.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleCameraController2d : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    
+
+    [Header("Parallax Layers")]
+    [SerializeField] private Transform farBackground, middleBackground;
+
+    private float lastXPos;
+
+    private void Start()
+    {
+        lastXPos = transform.position.x;
+    }
+
     void LateUpdate()
     {
         //Horizontal Movement
         transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        
+        float amountToMoveX = transform.position.x - lastXPos;
+
+        farBackground.position += new Vector3(amountToMoveX, 0, 0);
+        middleBackground.position += new Vector3(amountToMoveX*.5f,0,0);
+        
+        lastXPos = transform.position.x;
     }
 }
