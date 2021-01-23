3a3921819a5471288525bf1b294a52db06a0619d
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-23 16:45:28 -0500

    Simple horizontal 2d camera

---
+++ Assets/SimpleCameraController2d.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraController2d : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    
+    void LateUpdate()
+    {
+        //Horizontal Movement
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+    }
+}
