43175627702c5bcf195b74cfd755e61864fc8db1
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-23 00:32:48 -0500

    Player movement

---
+++ Assets/PlayerController2d.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2d : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rgb;
+
+    [Header("Property values")] 
+    [SerializeField] private float movementSpeed;
+
+    private int direction = 1;
+    
+    private void Awake()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+        FlipSprite();
+    }
+
+    void Movement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        if (horizontalMovement > 0)
+            direction = 1;
+        else if (horizontalMovement < 0)
+            direction = -1;
+        
+        rgb.velocity = new Vector2(horizontalMovement, rgb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        transform.eulerAngles = new Vector3(0, direction == 1 ? 0 : 180, 0);
+    }
+}
