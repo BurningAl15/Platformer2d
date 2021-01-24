9c2bf79adf55d4223d024c2c1fdfa6f6599b7447
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-23 23:27:42 -0500

    PlayerHealth - Damaging player

---
+++ Assets/DamagePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+            other.GetComponent<PlayerHealth>().DealDamage();
+    }
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int currentHealth, maxHealth;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage()
+    {
+        currentHealth--;
+        
+        //Validating
+        if(currentHealth<=0)
+            gameObject.SetActive(false);
+    }
+}
