7e1f539c2a56ab9c6e8bfd7968540c34a647dc2f
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-25 02:19:31 -0500

    Collect Effect

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -8,12 +8,15 @@ public class Pickup : MonoBehaviour
     public enum PickupType
     {
         Gem,
-        Coin,
         Health
     }
 
     public PickupType _pickupType;
 
+    [SerializeField] private GameObject effect;
+
+    private Coroutine currentCoroutine = null;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -23,8 +26,16 @@ public class Pickup : MonoBehaviour
             if(_pickupType==PickupType.Health)
                 PlayerHealth._instance.CureDamage();
             // if(_pickupType==PickupType.Coin)
-            
-            gameObject.SetActive(false);
+            if (currentCoroutine == null)
+                currentCoroutine = StartCoroutine(CollectEffect());
         }
+    }
+
+    IEnumerator CollectEffect()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        effect.SetActive(true);
+        yield return new WaitUntil(() => effect.GetComponent<TurnOff_OnTime>().finish);
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/TurnOff_OnTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOff_OnTime : MonoBehaviour
+{
+    public bool finish;
+    
+    public void TurnOnTimer()
+    {
+        gameObject.SetActive(false);
+        finish = true;
+    }
+}
