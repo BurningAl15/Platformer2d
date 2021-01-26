e8e488a14aa9a2ce04476233a8a48c7fabdb7a2c
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-01-26 18:36:54 -0500

    Frog enemy working - Enemy die effect optimized by not using Destroy

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rgb;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Animator anim;
+    
+    [Header("Movement Variables")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private int direction=-1;
+
+    [Header("Movement Limits")]
+    [SerializeField] private Transform leftPoint, rightPoint;
+
+    [SerializeField] private float moveTime, waitTime;
+    private float moveCount, waitCount;
+
+    [SerializeField] private GameObject deathEffect;
+
+    private bool isDeath = false;
+    private Coroutine currentCoroutine = null;
+    
+    void Awake()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
+        leftPoint.name = leftPoint.name + "- from -" + this.name;
+        rightPoint.name = rightPoint.name + "- from -" + this.name;
+        
+        leftPoint.parent = rightPoint.parent = null;
+
+        moveCount = moveTime;
+    }
+
+    void Update()
+    {
+        if(!isDeath)
+            Movement();
+        else
+            rgb.velocity = Vector2.zero;
+    }
+
+    void Movement()
+    {
+        if (moveCount > 0)
+        {
+            moveCount -= Time.deltaTime;
+            anim.SetBool("Moving", true);
+            rgb.velocity = new Vector2(moveSpeed * direction, rgb.velocity.y);
+
+            if (transform.position.x > rightPoint.position.x)
+                direction = -1;
+            else if (transform.position.x < leftPoint.position.x)
+                direction = 1;
+            
+            FlipSpriteRenderer();
+
+            if (moveCount <= 0)
+            {
+                waitCount = Random.Range(waitTime * .75f, waitTime * 1.25f);
+            }            
+        }
+        else if (waitCount > 0)
+        {
+            waitCount -= Time.deltaTime;
+
+            anim.SetBool("Moving", false);
+            rgb.velocity = new Vector2(0, rgb.velocity.y);
+
+            if (waitCount <= 0)
+            {
+                moveCount = Random.Range(moveTime * .75f, moveTime * 1.25f);
+            }
+        }
+    }
+
+    void FlipSpriteRenderer()
+    {
+        _spriteRenderer.flipX=direction!=-1?true:false;
+    }
+
+    public void Death()
+    {
+        if (currentCoroutine == null)
+            currentCoroutine = StartCoroutine(EnemyDie());
+    }
+
+    IEnumerator EnemyDie()
+    {
+        isDeath = true;
+        deathEffect.transform.position = _spriteRenderer.transform.position;
+        deathEffect.SetActive(true);
+        yield return new WaitUntil(() => deathEffect.GetComponent<TurnOff_OnTime>().finish);
+        gameObject.SetActive(false);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(.5f, .5f, .5f);
+        Gizmos.DrawSphere(leftPoint.position,.5f);
+        Gizmos.color = new Color(1f, 1f, 1f);
+        Gizmos.DrawSphere(rightPoint.position,.5f);
+    }
+}
---
+++ Assets/StompBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            print("Hit Enemy");
+            other.gameObject.SetActive(false);
+            
+            other.transform.parent.gameObject.GetComponent<EnemyController>().Death();
+        }
+    }
+}
